11d5e17b52cdbea40d7f49add0dca93b61b76bb4
Vlod <lokeshvengadabady@gmail.com>
2023-04-26 08:30:41 +0200

    26/04/23 8:30

---
+++ src/et3/java/geometrie/testClasseForme.java
@@ -0,0 +1,14 @@
+package et3.java.geometrie;
+
+import java.util.ArrayList;
+
+public class testClasseForme {
+    private ArrayList<Forme> formeListe;
+
+    public testClasseForme()
+    {
+        formeListe = new ArrayList<Forme>();
+
+    }
+
+}
